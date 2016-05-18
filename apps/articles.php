8c875338cea8e8ad409b71087fe4badc257284a1
cirelevuahc <eric.chauvel@neuf.fr>
2016-05-18 16:36:12 +0200

    version 1.1

--- apps/articles.php
+++ apps/articles.php
@@ -1,4 +1,24 @@
 <?php
-    if ( isset( $_SESSION['login'] ) && $_SESSION['login'] != '' )
-        require('views/articles.phtml');
+    // Get user order when display tache.
+    $order = 'priorite';
+
+    if ( isset( $_GET['order']) ) $order = $_GET['order'];
+
+    $option_order = array_fill( 0, 3, '' );
+
+    if ( $order == 'priorite') $option_order[0] = 'selected';
+    if ( $order == 'date-crea') $option_order[1] = 'selected';
+    if ( $order == 'date-rea') $option_order[2] = 'selected';
+    
+    // Get user order_by when display tache.
+    $order_by = 'asc';
+
+    if ( isset( $_GET['order-by']) ) $order_by = $_GET['order-by'];
+
+    $option_order_by = array_fill( 0, 2, '' );
+
+    if ( $order_by == 'asc') $option_order_by[0] = 'selected';
+    if ( $order_by == 'desc') $option_order_by[1] = 'selected';
+
+    require('views/articles.phtml');
 ?>
