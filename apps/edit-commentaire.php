06494f2c566c63aeb42211ce7412ca70905b7b8c
olivierdrg <olivierdrogue@gmail.com>
2016-05-23 15:52:00 +0200

    nouveau edit comm

--- apps/edit-commentaire.php
+++ apps/edit-commentaire.php
@@ -1,9 +1,3 @@
 <?php
-    if ( isset( $_SESSION['login'] ) && $_SESSION['login'] != '' ) {
-        require('views/edit-commentaire.phtml');
-    }
-
-    else {
-    	require('views/login.phtml');
-    }
+require('views/edit-commentaire.phtml');
 ?>
